edbc7e32ddff0d477a5d0ea9d0b920855d604769
SimonNyvall <simon.k.nyvall@gmail.com>
2023-03-27 17:30:16 +0200

    ToTitle

---
+++ ToTitle/ToTitle.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+
+namespace ToTitle;
+public static class StringExtension
+{
+    public static string ToTitle(this string text)
+    {
+        if (text.Length == 0)
+            throw new ArgumentException("Text cannot be empty", nameof(text));
+        
+        StringBuilder returnValue = new();
+        returnValue.Append(text[0].ToString().ToUpper());
+
+        if (text.Length == 1)
+        {
+            return returnValue.ToString();
+        }
+        
+        returnValue.Append(text[1..].ToLower());
+        return returnValue.ToString();
+    }
+}
