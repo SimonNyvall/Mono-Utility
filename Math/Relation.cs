2e7726304188b08571d49ea47aedfeda030100e2
SimonNyvall <simon.k.nyvall@gmail.com>
2023-03-28 08:10:00 +0200

    Added Denormalization function

--- Math/Relation.cs
+++ Math/Relation.cs
@@ -1,5 +1,15 @@
 ﻿namespace Math;
-public class Class1
+public class Relation
 {
+    public static double Normalization(double value, double min, double max)
+    {
+        return (value - min) / (max - min);
+    }
 
+    public static double Denormalization(double value, double min, double max)
+    {
+        return value * (max - min) + min;
+    }
+
+    
 }
