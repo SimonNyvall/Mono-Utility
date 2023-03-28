dfd9b570f1334f71b0bda3422603f82d5d4352ea
SimonNyvall <simon.k.nyvall@gmail.com>
2023-03-28 18:05:42 +0200

    Added List items to JsonCRUD instead of single use.

--- JsonCRUD/JsonDb.cs
+++ JsonCRUD/JsonDb.cs
@@ -3,39 +3,44 @@
 namespace JsonCRUD;
 public class JsonDb <T>
 {
-    private string _path;
-    private T _data;
+    private readonly List<string> _path = new();
+    private readonly List<T> _data = new();
     
     public JsonDb(string path)
     {
         if (!Path.IsPathRooted(path))
             throw new ArgumentException("Path must be rooted");
         
-        _path = path;
-        _data = JsonConvert.DeserializeObject<T>(File.ReadAllText(_path));
+        _path.Add(path);
+        _data.Add(JsonConvert.DeserializeObject<T>(File.ReadAllText(path)));
     }
 
-    public void CreateJson(T data)
+    public void AddPath(string path)
+    {
+        _path.Add(path);
+    }
+    
+    public void CreateJson(T data, int index)
     {
         string json = JsonConvert.SerializeObject(data, Formatting.Indented);
         
-        File.WriteAllText(_path, json);
+        File.WriteAllText(_path[index], json);
     }
     
-    public T ReadJson()
+    public T ReadJson(int index)
     {
-        return _data;
+        return _data[index];
     }
     
-    public void UpdateJson(T data)
+    public void UpdateJson(T data, int index)
     {
         string json = JsonConvert.SerializeObject(data, Formatting.Indented);
         
-        File.WriteAllText(_path, json);
+        File.WriteAllText(_path[index], json);
     }
     
-    public void DeleteJson()
+    public void DeleteJson(int index)
     {
-        File.Delete(_path);
+        File.Delete(_path[index]);
     }
 }
